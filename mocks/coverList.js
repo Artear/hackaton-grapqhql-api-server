6504e1e15f9650133d1c64e42b5a9f5221871582
leanazulyoro <leanazulyoro@gmail.com>
2017-10-06 15:56:04 -0300

    Added: cover to root schema

---
+++ mocks/coverList.js
@@ -0,0 +1,134 @@
+const CoverList = [
+  {
+  "section": "TodoNoticias",
+  "title": {
+    "main": "main title",
+    "social": "social title",
+    "headline": "headline title"
+  },
+  "containerList": [
+    {
+      "id": "=1212",
+      "type": "block",
+      "context": {
+        "type": "main"
+      },
+      "data": [
+        {
+          "id": "=11111",
+          "type": "article"
+        }
+      ]
+    },
+    {
+      "id": "=3434",
+      "type": "block",
+      "context": {
+        "type": "block_three"
+      },
+      "data": [
+        {
+          "id": "=22222",
+          "type": "article"
+        },
+        {
+          "id": "=33333",
+          "type": "media"
+        },
+        {
+          "id": "=44444",
+          "type": "article"
+        }
+      ]
+    }
+  ]
+},{
+  "section": "TNAutos",
+  "title": {
+    "main": "main title",
+    "social": "social title",
+    "headline": "headline title"
+  },
+  "containerList": [
+    {
+      "id": "=1212",
+      "type": "block",
+      "context": {
+        "type": "main"
+      },
+      "data": [
+        {
+          "id": "=11111",
+          "type": "article"
+        }
+      ]
+    },
+    {
+      "id": "=3434",
+      "type": "block",
+      "context": {
+        "type": "block_three"
+      },
+      "data": [
+        {
+          "id": "=22222",
+          "type": "article"
+        },
+        {
+          "id": "=33333",
+          "type": "media"
+        },
+        {
+          "id": "=44444",
+          "type": "article"
+        }
+      ]
+    }
+  ]
+},{
+  "section": "TNChicas",
+  "title": {
+    "main": "main title",
+    "social": "social title",
+    "headline": "headline title"
+  },
+  "containerList": [
+    {
+      "id": "=1212",
+      "type": "block",
+      "context": {
+        "type": "main"
+      },
+      "data": [
+        {
+          "id": "=11111",
+          "type": "article"
+        }
+      ]
+    },
+    {
+      "id": "=3434",
+      "type": "block",
+      "context": {
+        "type": "block_three"
+      },
+      "data": [
+        {
+          "id": "=22222",
+          "type": "article"
+        },
+        {
+          "id": "=33333",
+          "type": "media"
+        },
+        {
+          "id": "=44444",
+          "type": "article"
+        }
+      ]
+    }
+  ]
+},
+];
+
+module.exports = CoverList;
